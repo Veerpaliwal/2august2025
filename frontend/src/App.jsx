1e068a1e95ecebdff87c558917a59bf9fea62d66
Veerpaliwal <veerpaliwal901gmail.com>
2025-08-02 02:36:38 -0700

    new

---
+++ frontend/src/App.jsx
@@ -0,0 +1,41 @@
+
+//1. import area
+import { BrowserRouter, Routes, Route, } from "react-router-dom";
+import './App.css'
+import Layout from "./components/layouts/Layout";
+import Home from "./pages/Home";
+import Blogs from "./pages/Blogs";
+import Contact from "./pages/Contact";
+import NoPage from "./pages/NoPage";
+import { useState } from "react";
+
+
+//2. function definaction area
+function App() {
+
+  //2.1 hook variable / state variable
+  
+  //2.2 function defanction area
+
+  //2.3 return statement area
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout/>}>
+            <Route index element={<Home/>}/>
+            <Route path="blogs" element={<Blogs/>}/>
+            <Route path="contact" element={<Contact />} />
+          </Route>
+           <Route path="*" element={<NoPage/>}/>
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+//3. export area
+// name export
+// dafult area
+export default App
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,41 @@
+//1. IMPORT AREA
+import React, { useState } from "react";
+import TextField from "@mui/material/TextField"; 
+import Button from "@mui/material/Button";
+//2. FUNCTION DEFINACTION AREA
+ function Home() {
+    //2.1 HOOK VARIABLE
+     const [payload, setPlayload] = useState({
+                                    "data": {
+                                        "car_name": "",
+                                        "car_modal": "",
+                                    }
+                                });
+                                                     
+    //2.2 FUNCTION DAFINACTION AREA
+     // fat arrow function
+     let handleChange = (e) => {
+         console.log("hiii");
+         // object.memeber
+         //1. object.property
+         //2. object.method()
+         console.log(e.target?.value);
+     }
+
+     let saveData = () => {
+         console.log(payload);
+     }
+
+    //2.3 RETURN STATEMENT
+  return (
+    <>
+      <TextField onChange={handleChange} name="car_name" id="filled-basic" label="Enter Your Name" variant="filled" />
+       <br/>   
+      <TextField onChange={handleChange} name="car_modal" id="filled-basic" label="Enter Your Surname" variant="filled" />
+      <Button variant="contained" onClick={saveData}>submit</Button>
+    </>
+  );
+}
+
+//3. EXPORT AREA
+export default Home
